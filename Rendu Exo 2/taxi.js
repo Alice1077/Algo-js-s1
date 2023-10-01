7e44095b59bf8000cd5c0abddda8e8fe33ed0fbd
Alice107 <141587282+Alice1077@users.noreply.github.com>
2023-10-01 09:15:27 +0200

    Rendu Exercice 2

---
+++ Rendu Exo 2/taxi.js
@@ -0,0 +1,35 @@
+class tueur {
+    constructor(name, hp, probDeath, probAtk, probDeathWAtk) {
+    this.name = name
+    this.hp = hp
+    this.probDeath = probDeath
+    this.probAtk = probAtk
+    this.probDeathWAtk = probDeathWAtk
+    }
+}
+
+let Jason = new tueur("Jason", 100, 0.3, 0.5, 0.2)
+
+let characters = [
+    "Koralie",
+    "Sixtine",
+    "Louise",
+    "Janissa",
+    "Orphila"
+]
+
+let caracteristics = [
+    "Nerd",
+    "Sportif",
+    "Agile",
+    "Cours vite",
+    "Audacieux",
+    "Sang chaud"
+]
+
+/*Ici je pensais ajouter un randomItem pour que chaque personnage à une caractéristique*/
+
+
+while (Jason.hp >= 0) {
+
+}
